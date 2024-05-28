1cf8b22b5061f699960c35262b25e4998b682856
Nikhil Khot <nkhot2023+github@gmail.com>
2024-05-28 21:37:09 +0530

    Zombie movement and spawning

---
+++ GameDevFP/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+
+    CharacterController controller;
+    Transform player;
+    public float moveSpeed = 0.3f;
+    public float damageAmount = 5;
+    public float sightDist = 10;
+    bool playerSpotted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerVelocity = player.GetComponent<CharacterController>().velocity;
+        if (playerVelocity.magnitude < 0.001) {
+            Vector3 distance = player.position - transform.position;
+            playerSpotted = distance.magnitude < sightDist;
+            if (playerSpotted) {
+                Vector3 horzPos = player.position;
+                horzPos.y = transform.position.y;
+                transform.LookAt(horzPos);
+
+                Vector3 moveVector = distance;
+                moveVector.y = 0;
+                moveVector = moveVector.normalized * moveSpeed;
+
+                controller.Move(moveVector * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ GameDevFP/Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+
+    public GameObject zombiePrefab;
+    public int numToSpawn;
+    public float timeBetweenSpawns;
+    int numSpawned = 0;
+    float timeSinceLastSpawn = 0.0f;
+
+    public float xMin = -20;
+    public float xMax = 20;
+    public float yMin = 5;
+    public float yMax = 20;
+    public float zMin = -20;
+    public float zMax = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numSpawned < numToSpawn && timeSinceLastSpawn >= timeBetweenSpawns) {
+            spawnZombie();
+        }
+        timeSinceLastSpawn += Time.deltaTime;
+    }
+
+    void spawnZombie() {
+        Vector3 relativeZombiePosition;
+
+        relativeZombiePosition.x = Random.Range(xMin, xMax);
+        relativeZombiePosition.y = Random.Range(yMin, yMax);
+        relativeZombiePosition.z = Random.Range(zMin, zMax);
+
+        GameObject zombie = Instantiate(zombiePrefab, transform.position + relativeZombiePosition, transform.rotation)
+            as GameObject;
+
+        zombie.transform.parent = gameObject.transform;
+        numSpawned++;
+        timeSinceLastSpawn = 0.0f;
+    }
+}
